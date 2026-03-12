74d4ccf514262dcfbccc3b1267ebaa2823200588
fobdev <silvapedro55@gmail.com>
2026-03-12 20:02:23 -0300

    add vercel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,10 +13,15 @@ export const viewport: Viewport = {
   maximumScale: 1,
 }
 
+import { Analytics } from '@vercel/analytics/react'
+
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        {children}
+        <Analytics />
+      </body>
     </html>
   )
 }
